6c2d98368ffeceb6ded99d818d6bf70c7015187f
akashmitra <akash.mitra@outlook.com>
2014-05-03 12:05:45 +0530

    Version 1.5
    Cover pic calls from flickr now.

--- js/homepageslideshow.js
+++ js/homepageslideshow.js
@@ -3,6 +3,12 @@
 //Global Variables
 var i = 1;
 
+var linkforCoverimage = ["https://farm4.staticflickr.com/3714/9017437718_24d9760401_h.jpg",
+"https://farm3.staticflickr.com/2917/14089224754_48f8e30774_h.jpg",
+"https://farm8.staticflickr.com/7345/8841569270_6d9f731ea0_h.jpg",
+"https://farm6.staticflickr.com/5506/11064054416_e3c2ce2d54_h.jpg",
+"https://farm8.staticflickr.com/7318/9114860940_e62790b0cd_h.jpg"];
+
 $(document).ready(function() {
 
     setInterval(function() {
@@ -12,7 +18,7 @@ $(document).ready(function() {
 
     function changeCover() {
         
-        urlGen = "url(./images/intro-bg" + i + ".jpg)";
+        urlGen = "url("+linkforCoverimage[i]+")";
         console.log('urlGen :: ' + urlGen+':: Value of i ::'+i);
 
         $("#intropic").fadeOut('slow', function() {
