7ca9618d247165da802235d79191ab64b88b076b
akashmitra <akash.mitra@outlook.com>
2015-02-23 00:18:23 +0530

    UI changes
    Complete UI Change. Minimalistic Professional Design

---
+++ scripts/jquery.backtotop.js
@@ -0,0 +1,14 @@
+
+(function($) {
+    $.fn.backToTop = function() {
+        var selectz = $(this);
+        selectz.hide();
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > $(window).height()) {
+                selectz.fadeIn();
+            } else {
+                selectz.fadeOut();
+            }
+        });
+    };
+}(jQuery));
