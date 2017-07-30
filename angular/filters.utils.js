b1aba9bb391ff7ed1db5c25c9ba6c94972cda1d3
akashmitra <akash.mitra@outlook.com>
2017-07-30 23:08:01 +0530

    Images, Json & CSV, Filter

---
+++ angular/filters.utils.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+
+  angular.module('akashmitrapp')
+    .filter('customsort', function () {
+      return function (input, option1, option2) {
+        var output;
+
+      };
+
+    });
+
+}());
--- angular/landing.controller.js
+++ angular/landing.controller.js
@@ -10,11 +10,16 @@
   function LandingController($scope, dataservice) {
     var vm = $scope;
     vm.title = "Akash Mitra";
+    vm.filterCriteria = "star";
+
+    vm.setFilterCriteria = function (preferance) {
+      vm.filterCriteria = preferance;
+    };
 
     dataservice.getImageDetails().then(function (response) {
       console.log('Response :: ', response);
       vm.imageList = response.data;
-      
+
     }).catch(function (error) {
       console.log('Error :: ', error);
     });
