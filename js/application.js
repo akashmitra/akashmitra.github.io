3a825f4f9c0958bfd9395551dab5a88ebac6d1f1
akashmitra <akash.mitra@outlook.com>
2017-07-31 16:01:28 +0530

    Bug Fixes

--- js/application.js
+++ js/application.js
@@ -7,7 +7,8 @@
     $("<option />", {
        "selected": "selected",
        "value"   : "",
-       "text"    : "Navigation"
+       "text"    : "Navigation",
+       ""
     }).appendTo("#primary select");
     
     // populate dropdown
@@ -29,49 +30,17 @@
     // seperate nested menus
     $(".navigation li:has(ul)").css("margin-top", "25px");
 
-    // set default search text
-    $('#searchform input[type="text"]').attr('placeholder', 'Type to search...');
-
-    // portfolio masonry
-    var $container = $('.masonry');
-    $container.imagesLoaded( function(){
-        $container.isotope({
-            layoutMode: 'packery',
-            packery: {
-              columnWidth: '.masonry-item, .product'
-            },
-            itemSelector: '.masonry-item, .product'
-        });
-    });
-
-	// contact validation
-	$('#contact_form').validate({
-        rules: {
-            firstname    : 'required',
-            lastname     : 'required',
-            email : {
-                required : true,
-                email    : true
-            },
-            phone : {
-                required : true,
-                digits   : true
-            },
-            message      : 'required'
-        },
-        messages : {
-            firstname    : 'Looks like you forget to give us your first name.',
-            lastname     : 'Looks like you forget to give us your last name.',
-            email : {
-                required : 'Looks like you forget to give us your email address.',
-                email    : 'Looks like you may have made a typo. Email format is hello@example.com'
-            },
-            phone : {
-                required : 'Looks like you forget to give us your phone number.',
-                digits   : 'Numbers only, no spaces please.'
-            },
-            message      : 'Looks like you forget to say something.'
-        }
-    });
     
+    // portfolio masonry
+    // var $container = $('.masonry');
+    // $container.imagesLoaded( function(){
+    //     $container.isotope({
+    //         layoutMode: 'packery',
+    //         packery: {
+    //           columnWidth: '.masonry-item, .product'
+    //         },
+    //         itemSelector: '.masonry-item, .product'
+    //     });
+    // });
+
 })(jQuery);
