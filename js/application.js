3e670cafefe99575ea799a258e4da77b71f010d4
akashmitra <akash.mitra@outlook.com>
2017-08-09 01:29:53 +0530

    mobile fix- temp

--- js/application.js
+++ js/application.js
@@ -2,34 +2,35 @@
     "use strict";
     
     // responsive menu
-    $("<select />").appendTo("#primary");
+    // $("<select />").appendTo("#primary");
     
-    $("<option />", {
-       "selected": "selected",
-       "value"   : "",
-       "text"    : "Navigation"
-    }).appendTo("#primary select");
+    // $("<option />", {
+    //    "selected": "selected",
+    //    "value"   : "",
+    //    "text"    : "Navigation"
+    // }).appendTo("#primary select");
     
     // populate dropdown
-    $("#mainnav a").each(function(){
-     var el = $(this);
-     $("<option />", {
-         "value"   : el.attr("href"),
-         "text"    : el.text()
-     }).appendTo("#primary select");
-    });
+    // $("#mainnav a").each(function(){
+    //  var el = $(this);
+    //  $("<option />", {
+    //      "value"   : el.attr("href"),
+    //      "text"    : el.text(),
+    //      "ng-click" : el.attr("ng-click"),
+    //  }).appendTo("#primary select");
+    // });
     
     $("#primary select").change(function(){
         window.location = $(this).find("option:selected").val();
+
     });
 
 	// sidebar
-    $( ".open" ).pageslide({ direction: "left" });
+    //$( ".open" ).pageslide({ direction: "left" });
     
     // seperate nested menus
     $(".navigation li:has(ul)").css("margin-top", "25px");
 
-    
     // portfolio masonry
     // var $container = $('.masonry');
     // $container.imagesLoaded( function(){
